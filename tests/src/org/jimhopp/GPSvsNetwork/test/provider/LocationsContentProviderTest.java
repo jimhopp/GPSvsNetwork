ca650f8d8670279abe9e6a885db4ad6e3e812419
Jim Hopp <jimhopp@gmail.com>
2012-02-17 17:46:31 -0800

    start of content provider testing

---
+++ tests/src/org/jimhopp/GPSvsNetwork/test/provider/LocationsContentProviderTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.jimhopp.GPSvsNetwork.test.provider;
+
+import android.test.ProviderTestCase2;
+
+/**
+ * @author jhopp
+ *
+ */
+public class LocationsContentProviderTest extends ProviderTestCase2 {
+
+	/**
+	 * @param providerClass
+	 * @param providerAuthority
+	 */
+	public LocationsContentProviderTest(Class providerClass,
+			String providerAuthority) {
+		super(providerClass, providerAuthority);
+		// TODO Auto-generated constructor stub
+	}
+
+}
